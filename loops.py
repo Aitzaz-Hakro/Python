65e0f4928326d333beca778ca5e95c4150c8a388
Aitzaz Hassan <aitzazhakro123@gmail.com>
2024-12-07 15:25:31 +0500

    for loop

--- loops.py
+++ loops.py
@@ -1,6 +1,25 @@
 #while loops
 i=1
-while i<=10:
-    print(i)
-    i+=1
-print("loop ends")
+# while i<=10:
+#     print(i)
+#     i+=1
+# print("loop ends")
+
+#For LOOP
+# for letter in "Aitzaz Hassan":
+#     print(letter)
+
+ 
+# for i in range(10):  # incluede 1  excludes 10 
+#     print(i)
+
+for index in range(1,11,2): # (startpoint, endpoint, increament)
+    print(index)
+
+family=["aitzaz","nabeel", "adeel"]
+for ind in range(len(family)):
+    print(family[ind])
+ 
+
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,4 +1,17 @@
-ut("Enter Num1 ")
-# num2=input("Enter Num2 ")
-# result= int(num1) + int(num2)
-# print(result)
+#while loops
+i=1
+# while i<=10:
+#     print(i)
+#     i+=1
+# print("loop ends")
+
+#For LOOP
+# for letter in "Aitzaz Hassan":
+#     print(letter)
+
+ 
+# for i in range(10):  # incluede 1  excludes 10 
+#     print(i)
+
+for index in range(1,11,2): # (startpoint, endpoint, increament)
+    print(index)

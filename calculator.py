27707baddbc5b5790e13a4f0e3c8997e6c946c73
Aitzaz Hassan <aitzazhakro123@gmail.com>
2024-12-07 13:10:33 +0500

    completed control statements

--- calculator.py
+++ calculator.py
@@ -1,17 +1,23 @@
 from math import*
 
 def calculator(a,b,stringg):
-    if stringg=="-":
-        return a-b
-    elif stringg=="+":
-        return a+b
-    elif stringg=="*":
-        return a*b
-    elif stringg=="/":
-        return a/b
+    if stringg==1:
+        print(a+b)
+    elif stringg==2:
+        print(a-b)
+    elif stringg==3:
+        print(a*b)
+    elif stringg==4:
+        print(a/b)
     else:
        print("incorrect option")
        return
 
 def menu():
-    print("")
+    print("enter operation to perform \n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division")
+
+a=int(input("enter number1: "))
+b=int(input("enter number2: "))
+menu()
+oper=int(input("enter operation: "))
+calculator(a,b,oper)
---
+++ dictionary.py
@@ -0,0 +1,17 @@
+monthConvertion={
+    "Jan" : "January",
+    "Feb" : "Febuary",
+    "Mar" : "March",
+    "Apr" : "April",
+    "May" : "May",
+    "Jun" : "June",
+    "Jul" : "July",
+    "Aug" : "August",
+    "Sept" : "September",
+    "Oct" : "October",
+    "Nov" : "November",
+    "Dec" : "December",
+}
+
+print(monthConvertion["Apr"])
+print(monthConvertion.get("fri","Not a Valid Key"))

4d151e2d50b4846afefda5f4b17f0e926b16314a
Aitzaz Hassan <aitzazhakro123@gmail.com>
2024-12-11 22:43:47 +0500

    bank managment program

--- DS_Assignment01.py
+++ DS_Assignment01.py
@@ -1,7 +1,10 @@
-def Task01():
-#Task 1:
-# Write a function which take inputs(numbers) from user and prints its addition, multiplication, division and subtraction.
+import os
+os.system('cls')
 
+
+def Task01():
+# Write a function which take inputs(numbers) from user and prints its addition, multiplication, division and subtraction.
+ print("Task 1:\n")
  a=input("enter num1: ")        
  b=input("enter num2: ")        
  calculator(a,b)
@@ -15,9 +18,11 @@ def calculator(num1,num2):
     else:
         print("Diviion  is : ",(int(num1)/int(num2)))
 
+
+
 def Task02():
-# Task 2:
-# Write a program to calculatae the sum of all numbers in a given list using a for loop.
+# Write a program to calculatae the sum of all numbers in a given list using a for loop.*******************
+  print("Task 2:\n")
   Number_list=[4,5,3,64,9,2,14,43]
   Sum=0
   for index in range(len(Number_list)):
@@ -26,8 +31,10 @@ def Task02():
   print("\n",Number_list,"\nSum of all numbers in above list is:",Sum)
 
 
+
 def Task03():
-   #Find the smallest number in a list:
+   #Find the smallest number in a list:**********************************************************************
+   print("Task 3:\n")
    NumberList=[98,56,34,65,57,909,234,53,31,7]
    smallest=0
    for row in range(len(NumberList)):
@@ -36,27 +43,41 @@ def Task03():
             smallest=NumberList[row]
    print(smallest)
 
+
 def Task04():
-      for j in range(4):
+#write a program to print triangle of asterics(*) ***********************************************************
+   print("Task 4:\n")
+   for j in range(4):
          for k in range(j):
             print("*")
-      print("Null")
+     
+
 
 def Task05():
-  print("Null")
+ #write a program to print that prints multiplication table of any number b/w 1 and 10.**************************
+   print("Task 5:\n")
+   num=int(input("enter number to get his multiplication table: "))
+   for times in range(1,11):
+      print(times)
+
 
 def TaskList():
    print("1.Task1\n2.Task2\n3.Task3\n4.Task4")    
    a=int(input("Enter num: "))
    if a==1:
+      os.system('cls')
       Task01()
    elif a==2:
+      os.system('cls')
       Task02()  
    elif a==3:
+      os.system('cls')
       Task03()  
    elif a==4:
+      os.system('cls')
       Task04()  
    elif a==5:
+      os.system('cls')
       Task05()
    else:
       print("Invalid Iput please select b\w 1 and 5")

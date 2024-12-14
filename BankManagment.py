0fd30dd7b487937f5f0c60c32224d29ad9a58a95
Aitzaz Hassan <aitzazhakro123@gmail.com>
2024-12-14 17:30:20 +0500

    yt download

--- BankManagment.py
+++ BankManagment.py
@@ -1,5 +1,28 @@
 import os
+CurrentBalance:float = 0.0
 def MainMenu():
     os.system("cls") #clears the screen
-    print("")
-    
+    print("1.Check Balance\n2.Deposit Amount\n3.Withdraw Amount\n4.Exit")
+
+def CheckBalance():
+    print("Balance is: ",CurrentBalance)
+
+print(CurrentBalance)
+def Deposit():
+     
+     try:
+       amount=int(input("Enter Amount to Deposit: "))
+       if amount<=0:
+          print("cannot Deposited!!")
+       else:
+          CurrentBalance+=amount
+          print("Amount Deposited")
+          
+
+     except UnboundLocalError as err:
+         print(err)
+     except ValueError:
+      print("Incorrect Input")
+
+Deposit()
+print(CurrentBalance)

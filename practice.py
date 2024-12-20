4bfc719d77093b600854f6b2c52baa049012f73b
Aitzaz Hassan <aitzazhakro123@gmail.com>
2024-12-20 19:05:42 +0500

    ljsut() and rjust() functions

--- practice.py
+++ practice.py
@@ -1,3 +1,13 @@
+import os 
+os.system("cls")
+print("Welcome to the Calculator".rjust(20,"+"),"enter Num1")
+input("enter NUM1: ")
 
-print("Welcome to the Calculator")
+input("enter NUM2: ")
 
+def Menu():
+    print("Enter Operation.")
+    print("1.Add\n2.Subtract\n3.Divide\n4.multiply\n5.Square")
+
+
+Menu()
